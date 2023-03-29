fc02d27d0e0dd3390e972e1025dea48904d3e1bf
Azizul Hakim <azizulhakimgps@gmail.com>
2023-03-29 14:50:42 +0600

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
-import React from 'react';
-import './app.scss';
-import Navbar from './components/navbar/Navbar';
+import React from "react";
+import "./app.scss";
+import Navbar from "./components/navbar/Navbar";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 
 function App() {
+const Layout = () =>{
+  return (
+    <div className="app">
+      <Navbar/>
+      <Outlet/>
+      <Footer/>
+    </div>
+  )
+}
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Navbar />,
+    },
+  ]);
   return (
     <div className="App">
-      <Navbar/>
+      <RouterProvider router={router} />
     </div>
   );
 }
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,29 @@
 import React from "react";
 // import { Link } from "react-router-dom";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./Navbar.scss";
 
 const Navbar = () => {
   const [active, setActive] = useState(false);
+  const [open, setOpen] = useState(false);
+
+  const isActive = () => {
+    window.scrollY > 0 ? setActive(true) : setActive(false);
+  };
+  useEffect(() => {
+    window.addEventListener("scroll", isActive);
+    return () => {
+      window.removeEventListener("scroll", isActive);
+    };
+  }, []);
+
+  const currentUser = {
+    id: 1,
+    username: "Azizul",
+    image:
+      "https://scontent.fdac5-2.fna.fbcdn.net/v/t39.30808-6/279791909_1904530636403860_4967678786887752663_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=l8NXNoXFbAgAX-Bb9cH&_nc_ht=scontent.fdac5-2.fna&oh=00_AfD8ODUPV_FmsZ4wTnwIGMDCK8_iiYcjsO7S0_NEwxXk8A&oe=64299A8A",
+    isSeller: true,
+  };
   return (
     <div className={active ? "navbar active" : "navbar"}>
       <div className="container">
@@ -17,12 +36,29 @@ const Navbar = () => {
           <span>Fiverr Business</span>
           <span>Explore</span>
           <span>English</span>
-          <span>Become a Seller</span>
+          {!currentUser?.isSeller && <span>Become a Seller</span>}
           <span>Sign in</span>
-          <button>Join</button>
+          {!currentUser && <button>Join</button>}
+          {currentUser && (
+            <div className="user" onClick={()=>setOpen(!open)}>
+              <img src={currentUser.image} alt={currentUser.username} />
+              <span>{currentUser.username}</span>
+              {open && <div className="options">
+                {currentUser?.isSeller && (
+                  <>
+                    <span>Gigs</span>
+                    <span>Add New Gig</span>
+                    <span>Messages</span>
+                    <span>Orders</span>
+                    <span>Logout</span>
+                  </>
+                )}
+              </div>}
+            </div>
+          )}
         </div>
       </div>
-      {
+      {active && (
         <>
           <hr />
           <div className="menu">
@@ -30,7 +66,7 @@ const Navbar = () => {
             <span>Test</span>
           </div>
         </>
-      }
+      )}
     </div>
   );
 };

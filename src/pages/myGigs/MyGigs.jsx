67cc1a3d11815074cc618561bb66860812d4b2f2
Azizul Hakim <azizulhakimgps@gmail.com>
2023-03-30 18:34:42 +0600

    commit

--- src/pages/myGigs/MyGigs.jsx
+++ src/pages/myGigs/MyGigs.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React from "react";
+import "./MyGigs.scss";
+import { Link } from "react-router-dom";
 
 const MyGigs = () => {
   return (
-    <div>MyGigs</div>
-  )
-}
+    <div className="myGigs">
+      <div className="title">
+        <h1>Gigs</h1>
+        <Link to="/add" className="link">
+          Add New Gig
+        </Link>
+      </div>
+      <table>
+        <tr>
+          <th>Image</th>
+          <th>Title</th>
+          <th>Price</th>
+          <th>Sales</th>
+          <th>Action</th>
+        </tr>
+        <tr>
+          <td>
+            <img
+              className="image"
+              src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+              alt=""
+            />
+          </td>
+          <td>Stunning concept art</td>
+          <td>
+            59.<sup>99</sup>
+          </td>
+          <td>13</td>
+          <td>
+            <img className="delete" src="./img/delete.png" alt="" />
+          </td>
+        </tr>
+      </table>
+    </div>
+  );
+};
 
-export default MyGigs
+export default MyGigs;

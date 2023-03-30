38132f536997799148dcb2e1d31d8286f7928870
Azizul Hakim <azizulhakimgps@gmail.com>
2023-03-30 15:05:04 +0600

    commit

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,15 +1,98 @@
-import React from 'react'
-import "./Home.scss"
-import Featured from './../../components/featured/Featured';
-import TrustedBy from '../../components/trustedBy/TrustedBy';
-
+import React from "react";
+import "./Home.scss";
+import Featured from "./../../components/featured/Featured";
+import TrustedBy from "../../components/trustedBy/TrustedBy";
+import Slide from "../../components/slide/Slide";
+import { cards } from "../../data";
+import { projects } from "../../data"
+import CatCard from "../../components/catCard/CatCard";
+import ProjectCard from "../../components/projectCard/ProjectCard";
 const Home = () => {
   return (
     <div className="home">
-      <Featured/>
-      <TrustedBy/>
-    </div>
-  )
-}
+      <Featured />
+      <TrustedBy />
+      <Slide slidesToShow={5} arrowsScroll={5}>
+        {cards.map((card) => (
+          <CatCard item={card} key={card.id} />
+        ))}
+      </Slide>
+      <div className="features">
+        <div className="container">
+          <div className="item">
+            <h1>A whole world of freelance talent at your fingertips</h1>
+            <div className="title">
+              <img src="./img/check.png" alt="" />
+              The best for every budget
+            </div>
+            <p>
+              Find high-quality services at every price point. No hourly rates,
+              just project-based pricing.
+            </p>
 
-export default Home
+            <div className="title">
+              <img src="./img/check.png" alt="" />
+              Quality work done quickly
+            </div>
+            <p>
+            Find the right freelancer to begin working on your project within minutes.
+            </p>
+
+            <div className="title">
+              <img src="./img/check.png" alt="" />
+              Protected payments, every time
+            </div>
+            <p>
+            Always know what you'll pay upfront. Your payment isn't released until you approve the work.
+            </p>
+
+            <div className="title">
+              <img src="./img/check.png" alt="" />
+              24/7 support
+            </div>
+            <p>
+            Questions? Our round-the-clock support team is available to help anytime, anywhere.
+            </p>
+          </div>
+          <div className="item">
+            <video src="./img/video.mp4" controls poster="./img/thumbnail.jpg"></video>
+          </div>
+          
+        </div>
+      </div>
+      <div className="features dark">
+        <div className="container">
+          <div className="item">
+            <h1>fiver <b>Business</b></h1>
+            <h1>A business solution designed for <i>teams</i></h1>
+            <p>Upgrade to a curated experience packed with tools and benefits, dedicated to businesses</p>
+            <div className="title">
+              <img src="./img/check.png" alt="" />
+              Connect to freelancers with proven business experience
+            </div>
+            <div className="title">
+              <img src="./img/check.png" alt="" />
+              Get matched with the perfect talent by a customer success manager
+            </div>
+            <div className="title">
+              <img src="./img/check.png" alt="" />
+              Manage teamwork and boost productivity with one powerful workspace
+            </div>
+            <button>Explorer Fiverr Business</button>
+          </div>
+          <div className="item">
+            <img src="https://fiverr-res.cloudinary.com/q_auto,f_auto,w_585,dpr_1.0/v1/attachments/generic_asset/asset/d9c17ceebda44764b591a8074a898e63-1599597624769/business-desktop-585-x1.png" alt="images"/>
+          </div>
+          
+        </div>
+      </div>
+      <Slide slidesToShow={4} arrowsScroll={4}>
+        {projects.map((card) => (
+          <ProjectCard item={card} key={card.id} />
+        ))}
+      </Slide>
+    </div>
+  );
+};
+
+export default Home;

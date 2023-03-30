146927a9ecbd48b4778602e648d0e4cb8d255e23
Azizul Hakim <azizulhakimgps@gmail.com>
2023-03-30 11:28:10 +0600

    commit

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -6,12 +6,25 @@ const Featured = () => {
     <div className="featured">
       <div className="container">
         <div className="left">
-          <h1>Find the perfect freelance services for your business</h1>
+          <h1>Find the perfect <i>freelance</i> services for your business</h1>
           <div className="search">
-            
+            <div className="searchInput">
+              <img src="./img/search.png" alt="search" />
+              <input type="text" placeholder="Try building app" />
+            </div>
+            <button>Search</button>
+          </div>
+          <div className="popular">
+            <span>Popular:</span>
+            <button>Web Design</button>
+            <button>Wordpress</button>
+            <button>Logo Design</button>
+            <button>AI Services</button>
           </div>
         </div>
-        <div className="right"></div>
+        <div className="right">
+          <img src="./img/man.png" alt="man" />
+        </div>
       </div>
     </div>
   );
